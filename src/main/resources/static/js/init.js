778bff294f750eb339f0283440224d7d92ba0e26
tobia <tobia.92@hotmail.it>
2023-01-28 09:29:35 +0000

    first

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -14,3 +14,16 @@ function loadLevel(gameId, levelId) {
     loadHtmlPage('templates/level/level.html');
 
 }
+
+function loadGame(gameId, levelId) {
+  
+    $('#page-top').load("game.html", function () {
+        console.log("home loaded");
+        gameTracker = gameId;
+        levelTracker = Number(levelId);
+        loadHtmlPage('templates/level/level.html');
+    });
+
+   
+
+}
